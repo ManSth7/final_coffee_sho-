c7a2ba31e0ff4c61d60717face218f21b48845c0
Name <manoj.shrestha@aayulogic.com>
2019-09-27 22:18:27 +0545

    fixes

--- grunt/config/connect.js
+++ grunt/config/connect.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt, options) {
 
-  var port = grunt.option('port') || 9001;
-  var host = grunt.option('host') || "localhost";
+  var port = grunt.option('port') || 3000;
+  var host = grunt.option('host') || "54.205.249.200";
 
   return {
     server: {
